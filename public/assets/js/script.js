cd2b0f40400f222275e279b4233b38b28143b26e
EtienneGuignard <93521714+EtienneGuignard@users.noreply.github.com>
2022-08-02 16:21:37 +0200

    added slick plus layout main

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,40 +1,23 @@
 (function( $ ) {
-var deleteLink = $('.deleteButton')
 
-    $(document).ready(function() {
-        $(".alert-primary").hide();
-        deleteLink.on('click', function alertDelete() {
-            
-            if (confirm('êtes-vous sur de vouloir supprimer ceci')) {
-                $(".alert-primary").fadeTo(2000, 500).slideUp(500, function(){
-                    $(".alert-primary").slideUp(500);
-                });
+    $('.test').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 2000,
+                dots: true
+              }
             }
-        });
-    });
-    /*****home after registration****/ 
-    $(".registrationSuccess").fadeTo(5000, 500).slideUp(500, function(){
-        $(".registrationSuccess").slideUp(500);
-    });
-
-/***form** */
-    $(document).ready(function () { 
-        $('#zipCode').selectize({ sortField: 'text' }); 
-    });
-/***app flash error ****/
-$(".appFlashError").fadeTo(12000, 500).slideUp(500, function(){
-    $(".appFlashError").slideUp(500);
-});
-/***app flash success ****/
-$(".appFlashSuccess").fadeTo(3000, 500).slideUp(500, function(){
-    $(".appFlashSuccess").slideUp(500);
-})
-
-/**** ******/
-
-
-/****** autologin*/
-window.onload = function(){
-    document.forms['login_registration'].submit();
+      ]});
+                  
 }
-})( jQuery );
+)( jQuery );
